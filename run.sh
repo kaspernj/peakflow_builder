fda738aa385b7c212825f4697543141671db7a23
kaspernj <kaspernj@gmail.com>
2020-11-26 18:30:17 +0100

    Fixed dynamic path

--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ docker run -d \
   --net peak-flow-builder \
   --ip 56.0.0.2 \
   --publish 5676:5676 \
-  -v /home/kaspernj/Docker/peak-flow-builder/shared:/shared \
+  -v ./shared:/shared \
   docker:dind \
   dockerd-entrypoint.sh --group 1000 --tlsverify --tlscacert=/shared/certificates/tlscacert.pem --tlscert=/shared/certificates/tlscert.pem --tlskey=/shared/certificates/tlskey.pem -H tcp://0.0.0.0:5676
