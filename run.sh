867e52e329ccb34ae6e74c50ad74d3956ba693a7
kaspernj <k@spernj.org>
2022-07-18 09:28:25 +0200

    Use pwd

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ docker run -d \
   --name peakflow-builder \
   --net peakflow-builder \
   --ip 56.0.0.2 \
-  --publish 5676:5676 \
-  -v ./shared:/shared \
+  --publish 8676:8676 \
+  -v $(pwd)/shared:/shared \
   docker:dind \
-  dockerd-entrypoint.sh --group 1000 --tlsverify --tlscacert=/shared/certificates/tlscacert.pem --tlscert=/shared/certificates/tlscert.pem --tlskey=/shared/certificates/tlskey.pem -H tcp://0.0.0.0:5676
+  dockerd-entrypoint.sh --group 1000 --tlsverify --tlscacert=/shared/certificates/tlscacert.pem --tlscert=/shared/certificates/tlscert.pem --tlskey=/shared/certificates/tlskey.pem -H tcp://0.0.0.0:8676
