2d522020595aa37329b7b230117fca500897bce1
kaspernj <k@spernj.org>
2022-08-09 10:41:54 +0200

    Run certain version of Docker

--- run.sh
+++ run.sh
@@ -8,5 +8,5 @@ docker run -d \
   --ip 58.0.0.2 \
   --publish 8676:8676 \
   -v $(pwd)/shared:/shared \
-  docker:dind \
+  docker:20.10.10-dind \
   dockerd-entrypoint.sh --group 1000 --tlsverify --tlscacert=/shared/certificates/tlscacert.pem --tlscert=/shared/certificates/tlscert.pem --tlskey=/shared/certificates/tlskey.pem -H tcp://0.0.0.0:8676
