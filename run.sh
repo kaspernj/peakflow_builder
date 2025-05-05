6392a8ec025e17b3017e1b66d1ba164670b01507
kaspernj <kasper@diestoeckels.de>
2025-05-05 12:53:20 +0200

    Upgraded image

--- run.sh
+++ run.sh
@@ -8,5 +8,5 @@ docker run -d \
   --ip 58.0.0.2 \
   --publish 8676:8676 \
   -v $(pwd)/shared:/shared \
-  docker:27.0.3-dind \
+  docker:28.1.1-dind \
   dockerd-entrypoint.sh --group 1000 --tlsverify --tlscacert=/shared/certificates/tlscacert.pem --tlscert=/shared/certificates/tlscert.pem --tlskey=/shared/certificates/tlskey.pem -H tcp://0.0.0.0:8676
